0162634e11f6a7fc22fc811183d2fe4e22563038
Nandra Digital <iqbalrahayuddin155@gmail.com>
2025-10-31 12:10:12 +0700

    sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,11 +1,8 @@
-<!-- ========== Left Sidebar Start ========== -->
 <div class="sidebar-left">
 
     <div data-simplebar class="h-100">
 
-        <!--- Sidebar-menu -->
         <div id="sidebar-menu">
-            <!-- Left Menu Start -->
             <ul class="left-menu list-unstyled" id="side-menu">
                 <li>
                     <a href="dashboard" class="">
@@ -15,66 +12,18 @@
                 </li>
 
                 <li class="menu-title">TB Darussalam</li>
-<!-- masitkan sementara
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow ">
-                        <i class="fa fa-unlock-alt"></i>
-                        <span>Master Data</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="toko"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> toko</a></li>
-                        <li><a href="kategori"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Kategori</a></li>
-                        <li><a href="produk"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Produk</a></li>
-                    </ul>
+<li>
+                    <a href="pemasukan"><i class="fas fa-file-invoice-dollar"></i> <span>Input Pemasukan</span></a>
                 </li>
 
                 <li>
-                    <a href="javascript: void(0);" class="has-arrow ">
-                        <i class="fa fa-unlock-alt"></i>
-                        <span>Manajemen Stok</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="stok"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Stok</a></li>
-                        <li><a href="transfer-stok"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Transfer Stok</a></li>
-                        <li><a href="stok-masuk"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Stok Masuk</a></li>
-                    </ul>
+                    <a href="pengeluaran"><i class="fas fa-receipt"></i> <span>Input Pengeluaran</span></a>
                 </li>
 
                 <li>
-                    <a href="javascript: void(0);" class="has-arrow ">
-                        <i class="fa fa-unlink"></i>
-                        <span>Error</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="pages-404.php"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Error 404</a></li>
-                        <li><a href="pages-500.php"><i class="mdi mdi-checkbox-blank-circle align-middle"></i> Error 500</a></li>
-                    </ul>
+                    <a href="laporan-keuangan"><i class="fas fa-chart-line"></i> <span>Laporan Keuangan</span></a>
                 </li>
--->
-                <li>
-                    <a href="pemasukan"><i class="fas fa-pager"></i> <span>Input Pemasukan</span></a>
-                </li>
-
-                <li>
-                    <a href="pengeluaran"><i class="fas fa-pager"></i> <span>Input Pengeluaran</span></a>
-                </li>
-
-                <li>
-                    <a href="laporan-keuangan"><i class="fas fa-pager"></i> <span>Laporan Keuangan</span></a>
-                </li>
-<!-- Matikan Sementara
-                <li>
-                    <a href="pages-starter.php"><i class="fas fa-pager"></i> <span>Starter Page</span></a>
-                </li>
-
-                <li>
-                    <a href="pages-comingsoon.php"><i class="fas fa-tape"></i> <span>Coming Soon</span></a>
-                </li>
-
--->
-            </ul>
+</ul>
         </div>
-        <!-- Sidebar -->
-    </div>
+        </div>
 </div>
-<!-- Left Sidebar End -->
